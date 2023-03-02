37037fcfdbd499aeb12cd9311b50b99ede30f49f
elgizabbasov <lgzabbasov@gmail.com>
2023-03-01 22:07:29 -0700

    Initial Task model and backend setup

--- taskmanager/taskcatalog/models.py
+++ taskmanager/taskcatalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        HIGH = "high", "High"
+        MEDIUM = "medium", "Medium"
+        LOW = "low", "Low"
+    
+    class Status(models.TextChoices):
+        TODO = "to_do", "To-do"
+        INPROGRESS = "in_progress", "In Progress"
+        COMPLETED = "completed", "Completed"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    due_date = models.DateField(null=True)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.TODO
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('status',)
+        verbose_name = _("Task")
+        verbose_name_plural = _("Tasks")
+
+    def __str__(self):
+        return self.title
